a2326a3c1b4a47b460ad025a293a10b5820d9bfe
Dagar Davletov <dagar90@gmail.com>
2014-08-12 06:23:55 +0000

    Up to chapter 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  resources :microposts
+  match 'signup', to: "users#new", via: :get
+  root 'static_pages#home'
 
+  match 'help', to: 'static_pages#help', via: :get
+  match 'about', to: 'static_pages#about', via: :get
+  match 'contact', to: 'static_pages#contact', via: :get
+
+
+  resources :microposts
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
